98cac0709fafb12fca06f38f577277dc2b0f9dbd
Darwin Leung <darwin@squareup.com>
2024-08-12 14:25:07 -0400

    adding install package script

---
+++ tasks/install_packages.sh
@@ -0,0 +1,11 @@
+pip install numpy
+pip install pandas
+pip install koalas
+pip install logging
+pip install functools
+pip install --extra-index-url https://artifactory.global.square/artifactory/api/pypi/block-pypi/simple sq-pysnowflake
+pip install block-cloud-auth --extra-index-url=https://artifactory.global.square/artifactory/api/pypi/block-pypi/simple
+pip install --extra-index-url https://artifactory.global.square/artifactory/api/pypi/block-pypi/simple sq-pysnowflake dws_client
+
+
+pip install mkdocs
